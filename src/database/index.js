a822c56c342b5dee856e852fc1436284958233e5
Mücahit Nezir <mucahitnezir@gmail.com>
2020-05-03 23:53:30 +0300

    Auto import sequelize models

--- src/database/index.js
+++ src/database/index.js
@@ -1,3 +1,5 @@
+import fs from 'fs';
+import path from 'path';
 import { Sequelize } from 'sequelize';
 
 import * as config from '../config/sequelize';
@@ -6,8 +8,17 @@ import * as config from '../config/sequelize';
 const env = process.env.NODE_ENV || 'development';
 const sequelizeConfig = config[env];
 
+// Create sequelize instance
 const sequelize = new Sequelize(sequelizeConfig);
 
-sequelize.import('./models/user');
+// Import all model files
+const modelsPath = `${__dirname}/models`;
+
+fs
+  .readdirSync(modelsPath)
+  .filter((file) => (file.indexOf('.') !== 0) && (file.slice(-3) === '.js'))
+  .forEach((file) => {
+    sequelize.import(path.join(modelsPath, file));
+  });
 
 export default sequelize;
